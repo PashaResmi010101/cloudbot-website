3dae888c5a480ba88659c5cd6cc7d7e6aee94843
PashaResmi010101 <pashabuthack@gmail.com>
2024-08-11 16:05:48 +0300

    Update Header.jsx

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -13,7 +13,7 @@ const MobileNavbar = ({ open, setOpen, NavItems }) => {
 
                 <div className="flex justify-between border-b border-white/20 items-center px-5 py-4">
                     <div className="flex items-center">
-                        <img width="32" className="rounded-full icon-glow" src="public/img/logo2.png" />
+                        <img width="20" className="rounded-full icon-glow" src="public/img/logo2.png" />
                         <p className=" font-extrabold ml-1 text-2xl">Cloud BOT</p>
                     </div>
                     <button onClick={() => setOpen(!open)}><i className="cursor-pointer fa fa-times text-xl mr-2" /></button>
