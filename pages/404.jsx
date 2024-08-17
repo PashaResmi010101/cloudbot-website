31289d494ddd048cca41f34a02cdabf32960769b
PashaResmi010101 <pashabuthack@gmail.com>
2024-08-17 15:04:02 +0300

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -4,7 +4,8 @@ const UnkownPage = () => {
         'Have you lost your way, kiddo?',
         'Damn it! This dead-end road.',
         'The Aurors blocked this road, Harry!',
-        'Hey sen burada ne yapıyorsun çabuk çık buradan!'
+        'Hey sen burada ne yapıyorsun çabuk çık buradan!',
+        'Sayfa bulunamadı işte amk malı ne bekliyon hala burda?'
     ];
 
     return <ErrorPage code={404} message={messages[Math.floor(Math.random()*messages.length)] || "Sayfa bulunamadı ne bekliyon amk?"} />
